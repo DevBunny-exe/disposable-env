020061858a1d428f4bf1ad9ecc99a21ca8c349e9
DevBunny-exe <ronnycheng890@gmail.com>
2026-03-05 20:07:53 +0800

    Update client.py

--- sdk/disposable/client.py
+++ sdk/disposable/client.py
@@ -1,15 +1,7 @@
 import requests
-import sys
 
-API = "http://130.51.23.85:8000/run"
+API_URL = "http://130.51.23.85/run"
 
-def run(cmd):
-    r = requests.post(API, json={"script": cmd})
-    print(r.text)
-
-def main():
-    cmd = " ".join(sys.argv[1:])
-    run(cmd)
-
-if __name__ == "__main__":
-    main()
+def run(script):
+    r = requests.post(API_URL, json={"script": script})
+    return r.json()
